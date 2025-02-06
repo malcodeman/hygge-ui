0f776b17df10867837c75a55a7b4c6a860007f91
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-02-06 22:05:23 +0100

    feat: adds avatar component

---
+++ src/app/components/avatar.tsx
@@ -0,0 +1,26 @@
+import { Avatar as ArkAvatar } from "@ark-ui/react";
+import { cn } from "@/app/lib/cn";
+
+type Props = {
+  initials?: string;
+  src?: string | undefined;
+  alt?: string | undefined;
+  className?: string | undefined;
+};
+
+export const Avatar = (props: Props) => {
+  const { initials, src, alt, className, ...rest } = props;
+
+  return (
+    <ArkAvatar.Root
+      {...rest}
+      className={cn(
+        "flex size-8 items-center justify-center overflow-hidden rounded-full border border-[#E9E8E6] bg-[#f9f9f8] text-sm font-semibold text-[#21201C]",
+        className,
+      )}
+    >
+      {initials ? <ArkAvatar.Fallback>{initials}</ArkAvatar.Fallback> : null}
+      <ArkAvatar.Image src={src} alt={alt} />
+    </ArkAvatar.Root>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Avatar } from "./components/avatar";
+
 export default function Home() {
-  return <div className="text-2xl">ui</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="rounded-lg border border-[#E9E8E6] bg-[#F9F9F8] p-2">
+        <h2>Avatar</h2>
+        <Avatar initials="FB" src="https://i.pravatar.cc/300" alt="avatar" />
+      </div>
+    </div>
+  );
 }
