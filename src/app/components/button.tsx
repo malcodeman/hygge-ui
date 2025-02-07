a7a1a6244785d6b1dd559289491b5601a616e7ec
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-02-07 11:06:08 +0100

    feat: adds button component

---
+++ src/app/components/button.tsx
@@ -0,0 +1,106 @@
+import { LuLoaderCircle } from "react-icons/lu";
+import { cn } from "@/app/lib/cn";
+
+type ButtonVariant = "solid" | "outline";
+type ButtonColorPalette = "gray" | "orange" | "teal" | "green" | "red";
+type Props = {
+  loading?: boolean;
+  loadingText?: string;
+} & (
+  | {
+      colorPalette?: Extract<
+        ButtonColorPalette,
+        "orange" | "teal" | "green" | "red"
+      >;
+      variant?: Extract<ButtonVariant, "solid">;
+    }
+  | {
+      colorPalette?: Extract<ButtonColorPalette, "gray">;
+      variant?: ButtonVariant;
+    }
+) &
+  React.ComponentPropsWithoutRef<"button">;
+
+export function Button(props: Props) {
+  const {
+    loading,
+    loadingText,
+    colorPalette = "gray",
+    variant = "solid",
+    className,
+    children,
+    ...rest
+  } = props;
+  const disabled = loading || rest.disabled;
+  const isSolid = variant === "solid";
+  const isOutline = variant === "outline";
+
+  return (
+    <button
+      {...rest}
+      disabled={disabled}
+      className={cn(
+        className,
+        "inline-flex cursor-pointer items-center justify-center gap-1 rounded-lg border p-2 text-sm font-semibold transition-colors",
+        {
+          "cursor-default opacity-50": disabled,
+        },
+        {
+          "border-transparent bg-[#21201C] text-white":
+            colorPalette === "gray" && isSolid,
+        },
+        {
+          "border-transparent bg-[#eb5e41] text-white":
+            colorPalette === "orange" && isSolid,
+        },
+        {
+          "border-transparent bg-[#0d9488] text-white":
+            colorPalette === "teal" && isSolid,
+        },
+        {
+          "border-transparent bg-[#38a169] text-white":
+            colorPalette === "green" && isSolid,
+        },
+        {
+          "border-transparent bg-[#fd5454] text-white":
+            colorPalette === "red" && isSolid,
+        },
+        {
+          "hover:bg-[#21201C]/88":
+            colorPalette === "gray" && isSolid && !disabled,
+        },
+        {
+          "hover:bg-[#eb5e41]/88":
+            colorPalette === "orange" && isSolid && !disabled,
+        },
+        {
+          "hover:bg-[#0d9488]/88":
+            colorPalette === "teal" && isSolid && !disabled,
+        },
+        {
+          "hover:bg-[#38a169]/88":
+            colorPalette === "green" && isSolid && !disabled,
+        },
+        {
+          "hover:bg-[#fd5454]/88":
+            colorPalette === "red" && isSolid && !disabled,
+        },
+        {
+          "border-[#E9E8E6] text-[#63635E]": isOutline,
+        },
+        {
+          "hover:bg-[#EFEEEC] hover:text-[#21201C]": isOutline && !disabled,
+        },
+      )}
+    >
+      {loading ? (
+        <>
+          <LuLoaderCircle size={16} className="animate-spin" />
+          {loadingText ?? children}
+        </>
+      ) : (
+        children
+      )}
+    </button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { LuMail } from "react-icons/lu";
 import { Avatar } from "./components/avatar";
+import { Button } from "./components/button";
 import { Card } from "./components/card";
 import { Heading } from "./components/heading";
 
@@ -17,6 +19,67 @@ export default function Home() {
         <Heading level={4}>Heading 4</Heading>
         <Heading level={5}>Heading 5</Heading>
         <Heading level={6}>Heading 6</Heading>
+      </Card>
+      <Card>
+        <Heading level={2}>Button</Heading>
+        <div className="flex flex-col gap-2">
+          <Button colorPalette="gray" variant="solid">
+            Gray solid
+          </Button>
+          <Button colorPalette="gray" variant="outline">
+            Gray outline
+          </Button>
+          <Button colorPalette="orange">Orange</Button>
+          <Button colorPalette="teal">Teal</Button>
+          <Button colorPalette="green">Green</Button>
+          <Button colorPalette="red">Red</Button>
+          <Button loading loadingText="Gray solid loading...">
+            Gray solid loading
+          </Button>
+          <Button
+            variant="outline"
+            loading
+            loadingText="Gray outline loading..."
+          >
+            Gray outline loading
+          </Button>
+          <Button colorPalette="orange" loading loadingText="Orange loading...">
+            Orange loading
+          </Button>
+          <Button colorPalette="teal" loading loadingText="Teal loading...">
+            Teal loading
+          </Button>
+          <Button colorPalette="green" loading loadingText="Green loading...">
+            Green loading
+          </Button>
+          <Button colorPalette="red" loading loadingText="Red loading...">
+            Red loading
+          </Button>
+          <Button>
+            <LuMail size={16} />
+            With icon solid
+          </Button>
+          <Button variant="outline">
+            <LuMail size={16} />
+            With icon outline
+          </Button>
+          <Button colorPalette="orange">
+            <LuMail size={16} />
+            With icon orange
+          </Button>
+          <Button colorPalette="teal">
+            <LuMail size={16} />
+            With icon teal
+          </Button>
+          <Button colorPalette="green">
+            <LuMail size={16} />
+            With icon green
+          </Button>
+          <Button colorPalette="red">
+            <LuMail size={16} />
+            With icon red
+          </Button>
+        </div>
       </Card>
     </div>
   );
