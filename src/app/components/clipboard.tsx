add562d7b19e7df17b9929aa49e1354958b76a1e
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-02-11 12:31:55 +0100

    feat: improves clipboard component

--- src/app/components/clipboard.tsx
+++ src/app/components/clipboard.tsx
@@ -1,6 +1,8 @@
 import { Clipboard as ArkClipboard } from "@ark-ui/react";
 import { LuCheck, LuClipboardCopy } from "react-icons/lu";
 import { cn } from "@/app/lib/cn";
+import { Input } from "./input";
+import { Button } from "./button";
 
 type Props = {
   label?: React.ReactNode;
@@ -19,19 +21,15 @@ export function Clipboard(props: Props) {
         </ArkClipboard.Label>
       ) : null}
       <ArkClipboard.Control className={cn("flex gap-2")}>
-        <ArkClipboard.Input
-          className={cn(
-            "rounded-sm border border-[#E9E8E6] bg-[#f9f9f8] p-2 text-sm font-semibold text-[#21201C] focus:outline-2 focus:-outline-offset-1 focus:outline-[#21201C]",
-          )}
-        />
-        <ArkClipboard.Trigger
-          className={cn(
-            "cursor-pointer rounded-sm border border-[#E9E8E6] p-2 text-[#63635E] transition-colors hover:bg-[#EFEEEC] hover:text-[#21201C]",
-          )}
-        >
-          <ArkClipboard.Indicator copied={<LuCheck size={16} />}>
-            <LuClipboardCopy size={16} />
-          </ArkClipboard.Indicator>
+        <ArkClipboard.Input asChild>
+          <Input />
+        </ArkClipboard.Input>
+        <ArkClipboard.Trigger asChild>
+          <Button variant="outline">
+            <ArkClipboard.Indicator copied={<LuCheck size={16} />}>
+              <LuClipboardCopy size={16} />
+            </ArkClipboard.Indicator>
+          </Button>
         </ArkClipboard.Trigger>
       </ArkClipboard.Control>
     </ArkClipboard.Root>
