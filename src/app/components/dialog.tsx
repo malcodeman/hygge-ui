36e3965eb2b3e98e854af41d83783568010100e2
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-03-22 15:25:15 +0100

    feat: adds dialog dark mode support

--- src/app/components/dialog.tsx
+++ src/app/components/dialog.tsx
@@ -22,7 +22,7 @@ export function DialogContent(props: DialogContentProps) {
     <Portal>
       <ArkDialog.Backdrop
         className={cn(
-          "fixed top-0 left-0 z-50 h-dvh w-screen bg-white/80 blur-xs",
+          "fixed top-0 left-0 z-50 h-dvh w-screen bg-white/80 blur-xs dark:bg-black/80",
         )}
       />
       <ArkDialog.Positioner
@@ -33,7 +33,7 @@ export function DialogContent(props: DialogContentProps) {
         <ArkDialog.Content
           {...rest}
           className={cn(
-            "relative w-full max-w-lg rounded-lg bg-white shadow-[0_0_0_1px_#E9E8E6]",
+            "relative w-full max-w-lg rounded-lg bg-white shadow-[0_0_0_1px_#E9E8E6] dark:bg-[#191918] dark:shadow-[0_0_0_1px_#2a2a28]",
             className,
           )}
         >
@@ -60,7 +60,10 @@ export function DialogTitle(props: ArkDialog.TitleProps) {
   return (
     <ArkDialog.Title
       {...rest}
-      className={cn("text-2xl font-semibold text-[#21201C]", className)}
+      className={cn(
+        "text-2xl font-semibold text-[#21201C] dark:text-[#eeeeec]",
+        className,
+      )}
     />
   );
 }
@@ -71,7 +74,10 @@ export function DialogDescription(props: ArkDialog.DescriptionProps) {
   return (
     <ArkDialog.Description
       {...rest}
-      className={cn("text-sm font-semibold text-[#63635E]", className)}
+      className={cn(
+        "text-sm font-semibold text-[#63635E] dark:text-[#b5b3ad]",
+        className,
+      )}
     />
   );
 }
@@ -95,7 +101,7 @@ export function DialogFooter(props: React.ComponentPropsWithoutRef<"footer">) {
     <footer
       {...rest}
       className={cn(
-        "flex justify-between rounded-b-lg border-t border-t-[#E9E8E6] bg-[#f9f9f8] p-2",
+        "flex justify-between rounded-b-lg border-t border-t-[#E9E8E6] bg-[#f9f9f8] p-2 dark:border-t-[#2a2a28] dark:bg-[#222221]",
         className,
       )}
     />
