35528b116402a6df70611107258aceeabbdac323
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-08-02 15:25:16 +0200

    fix: toggle theme hydration

--- src/app/components/toggle-theme.tsx
+++ src/app/components/toggle-theme.tsx
@@ -1,17 +1,23 @@
 "use client";
 import { Button } from "@/components/button";
+import { useIsMounted } from "app/hooks/use-is-mounted";
 import { useTheme } from "next-themes";
 import { LuMoon, LuSun } from "react-icons/lu";
 
 export function ToggleTheme() {
   const { theme, setTheme } = useTheme();
+  const isMounted = useIsMounted();
 
-  return (
-    <Button
-      variant="ghost"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    >
-      {theme === "light" ? <LuSun size={16} /> : <LuMoon size={16} />}
-    </Button>
-  );
+  if (isMounted) {
+    return (
+      <Button
+        variant="ghost"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      >
+        {theme === "light" ? <LuSun size={16} /> : <LuMoon size={16} />}
+      </Button>
+    );
+  }
+
+  return <div className="size-[34px]" />;
 }
