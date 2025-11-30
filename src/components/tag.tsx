5357eb07cca491befcb55c03e28e641d2d3d0bf3
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-11-30 16:43:12 +0100

    feat: improves tag docs page

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -2,27 +2,29 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "./cn";
 
 const tagVariants = cva(
-  "border-border-subtle inline-flex items-center justify-center gap-2 rounded-sm border px-2",
+  "inline-flex items-center justify-center gap-2 rounded-sm border",
   {
     variants: {
       colorPalette: {
-        gray: `bg-[#efeeec] text-[#21201C] dark:bg-[#232322] dark:text-[#eeeeec]`,
-        orange: `bg-[#ffedd5] text-[#92310a] dark:bg-[#3b1106] dark:text-[#fdba74]`,
-        teal: `bg-[#ccfbf1] text-[#0c5d56] dark:bg-[#032726] dark:text-[#5eead4]`,
-        green: `bg-[#dcfce7] text-[#116932] dark:bg-[#042713] dark:text-[#86efac]`,
-        red: `bg-[#fee2e2] text-[#991919] dark:bg-[#300c0c] dark:text-[#fca5a5]`,
+        gray: "text-fg-default border-border-default bg-[#efeeec] dark:bg-[#232322]",
+        orange:
+          "border-[#f97316] bg-[#ffedd5] text-[#92310a] dark:border-[#fb923c] dark:bg-[#3b1106] dark:text-[#fdba74]",
+        teal: "border-[#14b8a6] bg-[#ccfbf1] text-[#0c5d56] dark:border-[#2dd4bf] dark:bg-[#032726] dark:text-[#5eead4]",
+        green:
+          "border-[#22c55e] bg-[#dcfce7] text-[#116932] dark:border-[#4ade80] dark:bg-[#042713] dark:text-[#86efac]",
+        red: "border-[#ef4444] bg-[#fee2e2] text-[#991919] dark:border-[#f87171] dark:bg-[#300c0c] dark:text-[#fca5a5]",
       },
       size: {
-        xs: "px-1 text-xs",
-        sm: "px-2 text-sm",
-        md: "px-2 text-base",
-        lg: "px-3 text-lg",
-        xl: "px-3 text-xl",
+        xs: "px-1 py-0.5 text-xs",
+        sm: "px-2 py-1 text-sm",
+        md: "px-2 py-1 text-base",
+        lg: "px-3 py-1.5 text-lg",
+        xl: "px-3 py-1.5 text-xl",
       },
     },
     defaultVariants: {
       colorPalette: "gray",
-      size: "md",
+      size: "xs",
     },
   },
 );
