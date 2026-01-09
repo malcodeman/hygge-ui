3f42b2c97dbd6f9c698178ae69f200eed7d1c75c
malcodeman <26983729+malcodeman@users.noreply.github.com>
2026-01-09 23:06:51 +0100

    feat: improves qr code docs page

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -1,5 +1,6 @@
 import { QrCode as ArkQrCode } from "@ark-ui/react";
 import { cva, VariantProps } from "class-variance-authority";
+import { LuLoaderCircle } from "react-icons/lu";
 import { cn } from "./cn";
 
 const qrCodeVariants = cva("flex flex-col gap-2", {
@@ -19,19 +20,38 @@ const qrCodeVariants = cva("flex flex-col gap-2", {
   },
 });
 
-type Props = ArkQrCode.RootProps & VariantProps<typeof qrCodeVariants>;
+type Props = ArkQrCode.RootProps & {
+  overlay?: React.ReactNode;
+  loading?: boolean;
+  fill?: string;
+} & VariantProps<typeof qrCodeVariants>;
 
 export function QrCode(props: Props) {
-  const { size, className, children, ...rest } = props;
+  const { overlay, loading, fill, size, className, children, ...rest } = props;
 
   return (
     <ArkQrCode.Root
-      className={cn(qrCodeVariants({ size }), className)}
+      className={cn("flex w-fit flex-col gap-2", className)}
       {...rest}
     >
-      <ArkQrCode.Frame className="bg-white">
-        <ArkQrCode.Pattern />
-      </ArkQrCode.Frame>
+      <div className="relative">
+        <ArkQrCode.Frame
+          className={cn("dark:fill-white", qrCodeVariants({ size }))}
+          style={{ fill }}
+        >
+          <ArkQrCode.Pattern />
+        </ArkQrCode.Frame>
+        {loading ? (
+          <div className="absolute inset-0 flex items-center justify-center bg-white/88 dark:bg-black/88">
+            <LuLoaderCircle size={16} className="animate-spin" />
+          </div>
+        ) : null}
+        {overlay && !loading ? (
+          <ArkQrCode.Overlay className="h-6 w-6 rounded-full outline-2 outline-white dark:outline-[#191918]">
+            {overlay}
+          </ArkQrCode.Overlay>
+        ) : null}
+      </div>
       {children}
     </ArkQrCode.Root>
   );
--- src/routes/docs.components.qr-code.tsx
+++ src/routes/docs.components.qr-code.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/button";
 import { Heading } from "@/components/heading";
-import { QrCode } from "@/components/qr-code";
+import { QrCode, QrCodeDownloadTrigger } from "@/components/qr-code";
 import { Code, Text } from "@/components/text";
 import { createFileRoute } from "@tanstack/react-router";
 import { PageHeader } from "app/components/page-header";
@@ -24,6 +25,7 @@ function QrCodePage() {
         description="A component that generates a QR code based on the provided data."
         githubLink="https://github.com/malcodeman/hygge-ui/blob/main/src/components/qr-code.tsx"
         className="mb-2"
+        verified
       />
       <PagePreviewCodeTabs
         preview={<QrCode value="https://www.hygge-ui.com" />}
@@ -41,24 +43,95 @@ function QrCodePage() {
       <PagePreviewCodeTabs
         preview={
           <div className="flex flex-col gap-2">
-            <QrCode size="2xs" />
-            <QrCode size="xs" />
-            <QrCode size="sm" />
-            <QrCode size="md" />
-            <QrCode size="lg" />
-            <QrCode size="xl" />
-            <QrCode size="2xl" />
+            <QrCode size="2xs" value="https://www.hygge-ui.com" />
+            <QrCode size="xs" value="https://www.hygge-ui.com" />
+            <QrCode size="sm" value="https://www.hygge-ui.com" />
+            <QrCode size="md" value="https://www.hygge-ui.com" />
+            <QrCode size="lg" value="https://www.hygge-ui.com" />
+            <QrCode size="xl" value="https://www.hygge-ui.com" />
+            <QrCode size="2xl" value="https://www.hygge-ui.com" />
           </div>
         }
         code={`<div className="flex flex-col gap-2">
-  <QrCode size="2xs" />
-  <QrCode size="xs" />
-  <QrCode size="sm" />
-  <QrCode size="md" />
-  <QrCode size="lg" />
-  <QrCode size="xl" />
-  <QrCode size="2xl" />
+  <QrCode size="2xs" value="https://www.hygge-ui.com" />
+  <QrCode size="xs" value="https://www.hygge-ui.com" />
+  <QrCode size="sm" value="https://www.hygge-ui.com" />
+  <QrCode size="md" value="https://www.hygge-ui.com" />
+  <QrCode size="lg" value="https://www.hygge-ui.com" />
+  <QrCode size="xl" value="https://www.hygge-ui.com" />
+  <QrCode size="2xl" value="https://www.hygge-ui.com" />
 </div>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Overlay
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>overlay</Code> prop to add a logo or overlay to the QR
+        code. This is useful when you want to brand the QR code.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <QrCode
+            value="https://www.hygge-ui.com"
+            overlay={
+              <img src="https://ark-ui.com/icon-192.png" alt="Ark UI Logo" />
+            }
+          />
+        }
+        code={`<QrCode
+  value="https://www.hygge-ui.com"
+  overlay={
+    <img src="https://ark-ui.com/icon-192.png" alt="Ark UI Logo" />
+  }
+/>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Fill
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>fill</Code> prop to set the fill color of the QR code.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={<QrCode value="https://www.hygge-ui.com" fill="#eb5e41" />}
+        code={`<QrCode value="https://www.hygge-ui.com" fill="#eb5e41" />`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Download
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>QrCodeDownloadTrigger</Code> to download the QR code.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <QrCode value="https://www.hygge-ui.com">
+            <QrCodeDownloadTrigger
+              asChild
+              fileName="qr-code.png"
+              mimeType="image/png"
+            >
+              <Button size="sm">Download</Button>
+            </QrCodeDownloadTrigger>
+          </QrCode>
+        }
+        code={`<QrCode value="https://www.hygge-ui.com">
+  <QrCodeDownloadTrigger
+    asChild
+    fileName="qr-code.png"
+    mimeType="image/png"
+  >
+    <Button size="sm">Download</Button>
+  </QrCodeDownloadTrigger>
+</QrCode>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Loading
+      </Heading>
+      <Text className="mb-2">
+        Pass the <Code>loading</Code> prop to show a spinner.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={<QrCode value="https://www.hygge-ui.com" loading />}
+        code={`<QrCode value="https://www.hygge-ui.com" loading />`}
       />
     </>
   );
