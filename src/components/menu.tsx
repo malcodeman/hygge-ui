a6dd2d39d8335c3b4765e34fd0decadda28c4c6c
malcodeman <26983729+malcodeman@users.noreply.github.com>
2026-01-30 19:32:46 +0100

    feat: improves menu component docs

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -5,8 +5,27 @@ export function MenuRoot(props: ArkMenu.RootProps) {
   return <ArkMenu.Root {...props} />;
 }
 
+export function MenuContextTrigger(props: ArkMenu.ContextTriggerProps) {
+  return <ArkMenu.ContextTrigger {...props} />;
+}
+
 export function MenuTrigger(props: ArkMenu.TriggerProps) {
   return <ArkMenu.Trigger {...props} />;
+}
+
+export function MenuTriggerItem(props: ArkMenu.TriggerItemProps) {
+  const { className, ...rest } = props;
+
+  return (
+    <ArkMenu.TriggerItem
+      {...rest}
+      className={cn(
+        "flex cursor-pointer items-center gap-1 rounded-sm px-2 py-1.5 text-sm font-medium transition-colors data-disabled:cursor-not-allowed data-disabled:opacity-50 data-disabled:hover:bg-transparent",
+        "text-fg-muted hover:bg-[#f1f0ef] hover:text-[#21201C] data-highlighted:bg-[#f1f0ef] data-highlighted:text-[#21201C] dark:hover:bg-[#2a2a28] dark:hover:text-[#eeeeec] dark:data-highlighted:bg-[#2a2a28] dark:data-highlighted:text-[#eeeeec]",
+        className,
+      )}
+    />
+  );
 }
 
 type MenuContentProps = {
@@ -21,13 +40,13 @@ export function MenuContent(props: MenuContentProps) {
       <ArkMenu.Content
         {...rest}
         className={cn(
-          "z-50 max-w-xs rounded-lg border border-[#E9E8E6] bg-white p-1.5 shadow-2xs focus:outline-2 focus:-outline-offset-1 dark:border-[#2a2a28] dark:bg-[#191918]",
+          "border-border-subtle z-50 max-w-xs rounded-lg border bg-white p-1.5 shadow-2xs dark:bg-[#191918]",
           className,
         )}
       >
         {showArrow ? (
           <ArkMenu.Arrow>
-            <ArkMenu.ArrowTip className="border-t border-l border-[#E9E8E6] dark:border-[#2a2a28]" />
+            <ArkMenu.ArrowTip className="border-border-subtle border-t border-l" />
           </ArkMenu.Arrow>
         ) : null}
         {children}
@@ -45,12 +64,23 @@ export function MenuItemGroup(props: ArkMenu.ItemGroupProps) {
       className={cn("flex flex-col gap-1", className)}
     >
       {title ? (
-        <ArkMenu.ItemGroupLabel className="px-2 py-1.5 text-sm font-semibold">
+        <ArkMenu.ItemGroupLabel className="px-2 py-1.5 text-sm/6 font-semibold">
           {title}
         </ArkMenu.ItemGroupLabel>
       ) : null}
       {children}
     </ArkMenu.ItemGroup>
+  );
+}
+
+export function MenuItemGroupLabel(props: ArkMenu.ItemGroupLabelProps) {
+  const { className, ...rest } = props;
+
+  return (
+    <ArkMenu.ItemGroupLabel
+      {...rest}
+      className={cn("text-fg-default px-2 text-sm/6 font-semibold", className)}
+    />
   );
 }
 
@@ -62,7 +92,7 @@ export function MenuItem(props: ArkMenu.ItemProps) {
       {...rest}
       className={cn(
         "flex cursor-pointer items-center gap-1 rounded-sm px-2 py-1.5 text-sm font-medium transition-colors data-disabled:cursor-not-allowed data-disabled:opacity-50 data-disabled:hover:bg-transparent",
-        "text-[#63635E] hover:bg-[#f1f0ef] hover:text-[#21201C] data-highlighted:bg-[#f1f0ef] data-highlighted:text-[#21201C] dark:text-[#b5b3ad] dark:hover:bg-[#2a2a28] dark:hover:text-[#eeeeec] dark:data-highlighted:bg-[#2a2a28] dark:data-highlighted:text-[#eeeeec]",
+        "text-fg-muted hover:bg-[#f1f0ef] hover:text-[#21201C] data-highlighted:bg-[#f1f0ef] data-highlighted:text-[#21201C] dark:hover:bg-[#2a2a28] dark:hover:text-[#eeeeec] dark:data-highlighted:bg-[#2a2a28] dark:data-highlighted:text-[#eeeeec]",
         className,
       )}
     />
@@ -75,10 +105,7 @@ export function MenuSeparator(props: ArkMenu.SeparatorProps) {
   return (
     <ArkMenu.Separator
       {...rest}
-      className={cn(
-        "-mx-1.5 my-1 border-[#E9E8E6] dark:border-[#2a2a28]",
-        className,
-      )}
+      className={cn("border-border-subtle -mx-1.5 my-1", className)}
     />
   );
 }
--- src/routes/docs.components.menu.tsx
+++ src/routes/docs.components.menu.tsx
@@ -1,13 +1,23 @@
+import { Avatar } from "@/components/avatar";
 import { Button } from "@/components/button";
+import { Heading } from "@/components/heading";
 import {
   MenuContent,
+  MenuContextTrigger,
   MenuItem,
+  MenuItemGroup,
+  MenuItemGroupLabel,
   MenuRoot,
+  MenuSeparator,
   MenuTrigger,
+  MenuTriggerItem,
 } from "@/components/menu";
+import { Code, Text } from "@/components/text";
+import { Portal } from "@ark-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
 import { PageHeader } from "app/components/page-header";
 import { PagePreviewCodeTabs } from "app/components/page-preview-code-tabs";
+import { LuChevronRight } from "react-icons/lu";
 
 export const Route = createFileRoute("/docs/components/menu")({
   component: MenuPage,
@@ -28,10 +38,11 @@ function MenuPage() {
         description="Used to create an accessible dropdown menu."
         githubLink="https://github.com/malcodeman/hygge-ui/blob/main/src/components/menu.tsx"
         className="mb-2"
+        verified
       />
       <PagePreviewCodeTabs
         preview={
-          <MenuRoot positioning={{ placement: "bottom" }}>
+          <MenuRoot>
             <MenuTrigger asChild>
               <Button variant="outline" size="sm">
                 Open
@@ -46,7 +57,7 @@ function MenuPage() {
             </MenuContent>
           </MenuRoot>
         }
-        code={`<MenuRoot positioning={{ placement: "bottom" }}>
+        code={`<MenuRoot>
   <MenuTrigger asChild>
     <Button variant="outline" size="sm">
       Open
@@ -58,6 +69,296 @@ function MenuPage() {
     <MenuItem value="new-win">New Window</MenuItem>
     <MenuItem value="open-file">Open File...</MenuItem>
     <MenuItem value="export">Export</MenuItem>
+  </MenuContent>
+</MenuRoot>`}
+      />
+      <Heading level={2} className="mt-10 mb-2">
+        Examples
+      </Heading>
+      <Heading level={3} size="lg" className="mb-2">
+        Context Menu
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>MenuContextTrigger</Code> component to create a context
+        menu.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <MenuRoot>
+            <MenuContextTrigger className="w-full">
+              <div className="border-border-subtle flex h-40 items-center justify-center rounded-lg border-2 border-dashed p-4">
+                Right click here
+              </div>
+            </MenuContextTrigger>
+            <MenuContent>
+              <MenuItem value="new-txt">New Text File</MenuItem>
+              <MenuItem value="new-file">New File...</MenuItem>
+              <MenuItem value="new-win">New Window</MenuItem>
+              <MenuItem value="open-file">Open File...</MenuItem>
+              <MenuItem value="export">Export</MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        }
+        code={`<MenuRoot>
+  <MenuContextTrigger className="w-full">
+    <div className="border-border-subtle flex h-40 items-center justify-center rounded-lg border-2 border-dashed p-4">
+      Right click here
+    </div>
+  </MenuContextTrigger>
+  <MenuContent>
+    <MenuItem value="new-txt">New Text File</MenuItem>
+    <MenuItem value="new-file">New File...</MenuItem>
+    <MenuItem value="new-win">New Window</MenuItem>
+    <MenuItem value="open-file">Open File...</MenuItem>
+    <MenuItem value="export">Export</MenuItem>
+  </MenuContent>
+</MenuRoot>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Group
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>MenuItemGroup</Code> component to group related menu
+        items.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <MenuRoot>
+            <MenuTrigger asChild>
+              <Button variant="outline" size="sm">
+                Edit
+              </Button>
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItemGroup>
+                <MenuItemGroupLabel>Styles</MenuItemGroupLabel>
+                <MenuItem value="bold">Bold</MenuItem>
+                <MenuItem value="underline">Underline</MenuItem>
+              </MenuItemGroup>
+              <MenuSeparator />
+              <MenuItemGroup>
+                <MenuItemGroupLabel>Align</MenuItemGroupLabel>
+                <MenuItem value="left">Left</MenuItem>
+                <MenuItem value="middle">Middle</MenuItem>
+                <MenuItem value="right">Right</MenuItem>
+              </MenuItemGroup>
+            </MenuContent>
+          </MenuRoot>
+        }
+        code={`<MenuRoot>
+  <MenuTrigger asChild>
+    <Button variant="outline" size="sm">
+      Edit
+    </Button>
+  </MenuTrigger>
+  <MenuContent>
+    <MenuItemGroup>
+      <MenuItemGroupLabel>Styles</MenuItemGroupLabel>
+      <MenuItem value="bold">Bold</MenuItem>
+      <MenuItem value="underline">Underline</MenuItem>
+    </MenuItemGroup>
+    <MenuSeparator />
+    <MenuItemGroup>
+      <MenuItemGroupLabel>Align</MenuItemGroupLabel>
+      <MenuItem value="left">Left</MenuItem>
+      <MenuItem value="middle">Middle</MenuItem>
+      <MenuItem value="right">Right</MenuItem>
+    </MenuItemGroup>
+  </MenuContent>
+</MenuRoot>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Submenu
+      </Heading>
+      <Text className="mb-2">
+        Here&apos;s an example of how to create a submenu.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <MenuRoot positioning={{ placement: "right-start" }}>
+            <MenuTrigger asChild>
+              <Button variant="outline" size="sm">
+                Open
+              </Button>
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItem value="new-txt">New Text File</MenuItem>
+              <MenuItem value="new-file">New File...</MenuItem>
+              <MenuRoot positioning={{ placement: "right-start", gutter: 2 }}>
+                <MenuTriggerItem>
+                  Open Recent <LuChevronRight />
+                </MenuTriggerItem>
+                <Portal>
+                  <MenuContent>
+                    <MenuItem value="panda">Panda</MenuItem>
+                    <MenuItem value="ark">Ark UI</MenuItem>
+                    <MenuItem value="chakra">Chakra v3</MenuItem>
+                  </MenuContent>
+                </Portal>
+              </MenuRoot>
+              <MenuItem value="open-file">Open File...</MenuItem>
+              <MenuItem value="export">Export</MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        }
+        code={`<MenuRoot positioning={{ placement: "right-start" }}>
+  <MenuTrigger asChild>
+    <Button variant="outline" size="sm">
+      Open
+    </Button>
+  </MenuTrigger>
+  <MenuContent>
+    <MenuItem value="new-txt">New Text File</MenuItem>
+    <MenuItem value="new-file">New File...</MenuItem>
+    <MenuRoot positioning={{ placement: "right-start", gutter: 2 }}>
+      <MenuTriggerItem>
+        Open Recent <LuChevronRight />
+      </MenuTriggerItem>
+      <Portal>
+        <MenuContent>
+          <MenuItem value="panda">Panda</MenuItem>
+          <MenuItem value="ark">Ark UI</MenuItem>
+          <MenuItem value="chakra">Chakra v3</MenuItem>
+        </MenuContent>
+      </Portal>
+    </MenuRoot>
+    <MenuItem value="open-file">Open File...</MenuItem>
+    <MenuItem value="export">Export</MenuItem>
+  </MenuContent>
+</MenuRoot>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Links
+      </Heading>
+      <Text className="mb-2">
+        Pass the <Code>asChild</Code> prop to the <Code>MenuItem</Code>{" "}
+        component to render a link.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <MenuRoot>
+            <MenuTrigger asChild>
+              <Button variant="outline" size="sm">
+                Select Anime
+              </Button>
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItem value="Naruto" asChild>
+                <a href="https://www.crunchyroll.com/naruto" target="_blank">
+                  Naruto
+                </a>
+              </MenuItem>
+              <MenuItem value="One Piece" asChild>
+                <a href="https://www.crunchyroll.com/one-piece" target="_blank">
+                  One Piece
+                </a>
+              </MenuItem>
+              <MenuItem value="Attack on Titan" asChild>
+                <a
+                  href="https://www.crunchyroll.com/attack-on-titan"
+                  target="_blank"
+                >
+                  Attack on Titan
+                </a>
+              </MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        }
+        code={`<MenuRoot>
+  <MenuTrigger asChild>
+    <Button variant="outline" size="sm">
+      Select Anime
+    </Button>
+  </MenuTrigger>
+  <MenuContent>
+    <MenuItem value="Naruto" asChild>
+      <a href="https://www.crunchyroll.com/naruto" target="_blank">
+        Naruto
+      </a>
+    </MenuItem>
+    <MenuItem value="One Piece" asChild>
+      <a href="https://www.crunchyroll.com/one-piece" target="_blank">
+        One Piece
+      </a>
+    </MenuItem>
+    <MenuItem value="Attack on Titan" asChild>
+      <a
+        href="https://www.crunchyroll.com/attack-on-titan"
+        target="_blank"
+      >
+        Attack on Titan
+      </a>
+    </MenuItem>
+  </MenuContent>
+</MenuRoot>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Placement
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>placement</Code> prop to control the placement of the
+        menu.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <MenuRoot positioning={{ placement: "right-start" }}>
+            <MenuTrigger asChild>
+              <Button variant="outline" size="sm">
+                Open
+              </Button>
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItem value="new-txt">New Text File</MenuItem>
+              <MenuItem value="new-file">New File...</MenuItem>
+              <MenuItem value="new-win">New Window</MenuItem>
+              <MenuItem value="open-file">Open File...</MenuItem>
+              <MenuItem value="export">Export</MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        }
+        code={`<MenuRoot positioning={{ placement: "right-start" }}>
+  <MenuTrigger asChild>
+    <Button variant="outline" size="sm">
+      Open
+    </Button>
+  </MenuTrigger>
+  <MenuContent>
+    <MenuItem value="new-txt">New Text File</MenuItem>
+    <MenuItem value="new-file">New File...</MenuItem>
+    <MenuItem value="new-win">New Window</MenuItem>
+    <MenuItem value="open-file">Open File...</MenuItem>
+    <MenuItem value="export">Export</MenuItem>
+  </MenuContent>
+</MenuRoot>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Avatar
+      </Heading>
+      <Text className="mb-2">
+        Here&apos;s an example that composes the <Code>Menu</Code> with the{" "}
+        <Code>Avatar</Code> component to display a menu underneath an avatar.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <MenuRoot positioning={{ placement: "right-end" }}>
+            <MenuTrigger>
+              <Avatar size="sm" src="https://bit.ly/sage-adebayo" />
+            </MenuTrigger>
+            <MenuContent>
+              <MenuItem value="account">Account</MenuItem>
+              <MenuItem value="settings">Settings</MenuItem>
+              <MenuItem value="logout">Logout</MenuItem>
+            </MenuContent>
+          </MenuRoot>
+        }
+        code={`<MenuRoot positioning={{ placement: "right-end" }}>
+  <MenuTrigger>
+    <Avatar size="sm" src="https://bit.ly/sage-adebayo" />
+  </MenuTrigger>
+  <MenuContent>
+    <MenuItem value="account">Account</MenuItem>
+    <MenuItem value="settings">Settings</MenuItem>
+    <MenuItem value="logout">Logout</MenuItem>
   </MenuContent>
 </MenuRoot>`}
       />
