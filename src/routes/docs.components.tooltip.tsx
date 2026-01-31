d5e8cfc86a1c109d13b6f6c6da194738fc697906
malcodeman <26983729+malcodeman@users.noreply.github.com>
2026-01-31 01:14:32 +0100

    feat: improves tooltip docs

--- src/routes/docs.components.tooltip.tsx
+++ src/routes/docs.components.tooltip.tsx
@@ -1,8 +1,13 @@
+import { Avatar } from "@/components/avatar";
 import { Button } from "@/components/button";
+import { Checkbox } from "@/components/checkbox";
+import { Heading } from "@/components/heading";
+import { Code, Text } from "@/components/text";
 import { Tooltip } from "@/components/tooltip";
 import { createFileRoute } from "@tanstack/react-router";
 import { PageHeader } from "app/components/page-header";
 import { PagePreviewCodeTabs } from "app/components/page-preview-code-tabs";
+import { useId } from "react";
 
 export const Route = createFileRoute("/docs/components/tooltip")({
   component: TooltipPage,
@@ -16,6 +21,9 @@ export const Route = createFileRoute("/docs/components/tooltip")({
 });
 
 function TooltipPage() {
+  const avatarId = useId();
+  const checkboxId = useId();
+
   return (
     <>
       <PageHeader
@@ -23,6 +31,7 @@ function TooltipPage() {
         description="Used to display additional information when a user hovers over an element."
         githubLink="https://github.com/malcodeman/hygge-ui/blob/main/src/components/tooltip.tsx"
         className="mb-2"
+        verified
       />
       <PagePreviewCodeTabs
         preview={
@@ -36,6 +45,207 @@ function TooltipPage() {
   <Button variant="outline" size="sm">
     Hover me
   </Button>
+</Tooltip>`}
+      />
+      <Heading level={2} className="mt-10 mb-2">
+        Examples
+      </Heading>
+      <Heading level={3} size="lg" className="mb-2">
+        Arrow
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>showArrow</Code> prop to show an arrow on the tooltip.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip showArrow content="This is the tooltip content">
+            <Button variant="outline" size="sm">
+              Hover me
+            </Button>
+          </Tooltip>
+        }
+        code={`<Tooltip showArrow content="This is the tooltip content">
+  <Button variant="outline" size="sm">
+    Hover me
+  </Button>
+</Tooltip>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Placement
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>positioning.placement</Code> prop to change the position
+        of the tooltip.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip
+            positioning={{ placement: "right-end" }}
+            content="This is the tooltip content"
+          >
+            <Button variant="outline" size="sm">
+              Hover me
+            </Button>
+          </Tooltip>
+        }
+        code={`<Tooltip
+  positioning={{ placement: "right-end" }}
+  content="This is the tooltip content"
+>
+  <Button variant="outline" size="sm">
+    Hover me
+  </Button>
+</Tooltip>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Offset
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>positioning.offset</Code> prop to change the offset of the
+        tooltip.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip
+            positioning={{ offset: { mainAxis: 4, crossAxis: 4 } }}
+            content="This is the tooltip content"
+          >
+            <Button variant="outline" size="sm">
+              Hover me
+            </Button>
+          </Tooltip>
+        }
+        code={`<Tooltip
+  positioning={{ offset: { mainAxis: 4, crossAxis: 4 } }}
+  content="This is the tooltip content"
+>
+  <Button variant="outline" size="sm">
+    Hover me
+  </Button>
+</Tooltip>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Delay
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>openDelay</Code> and <Code>closeDelay</Code> prop to
+        change the delay of the tooltip.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip
+            openDelay={500}
+            closeDelay={100}
+            content="This is the tooltip content"
+          >
+            <Button variant="outline" size="sm">
+              Delay (open: 500ms, close: 100ms)
+            </Button>
+          </Tooltip>
+        }
+        code={`<Tooltip
+  openDelay={500}
+  closeDelay={100}
+  content="This is the tooltip content"
+>
+  <Button variant="outline" size="sm">
+    Delay (open: 500ms, close: 100ms)
+  </Button>
+</Tooltip>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Interactive
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>interactive</Code> prop to keep the tooltip open when
+        interacting with its content.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip interactive content="This is the tooltip content">
+            <Button variant="outline" size="sm">
+              Hover me
+            </Button>
+          </Tooltip>
+        }
+        code={`<Tooltip interactive content="This is the tooltip content">
+  <Button variant="outline" size="sm">
+    Hover me
+  </Button>
+</Tooltip>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Disable
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>disabled</Code> prop to disable the tooltip. When
+        disabled, the tooltip will not be shown.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip disabled content="This is the tooltip content">
+            <Button variant="outline" size="sm">
+              Hover me
+            </Button>
+          </Tooltip>
+        }
+        code={`<Tooltip disabled content="This is the tooltip content">
+  <Button variant="outline" size="sm">
+    Hover me
+  </Button>
+</Tooltip>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        With Avatar
+      </Heading>
+      <Text className="mb-2">
+        Here&apos;s an example of how to use the <Code>Tooltip</Code>
+        with an
+        <Code>Avatar</Code> component.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip
+            ids={{ trigger: avatarId }}
+            content="Segun Adebayo is online"
+          >
+            <Avatar
+              ids={{ root: avatarId }}
+              src="https://bit.ly/sage-adebayo"
+            />
+          </Tooltip>
+        }
+        code={`<Tooltip
+  ids={{ trigger: avatarId }}
+  content="Segun Adebayo is online"
+>
+  <Avatar
+    ids={{ root: avatarId }}
+    src="https://bit.ly/sage-adebayo"
+  />
+</Tooltip>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        With Checkbox
+      </Heading>
+      <Text className="mb-2">
+        Here&apos;s an example of how to use the <Code>Tooltip</Code>
+        with a<Code>Checkbox</Code> component.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <Tooltip
+            ids={{ trigger: checkboxId }}
+            content="This is the tooltip content"
+          >
+            <Checkbox ids={{ root: checkboxId }} label="Welcome" />
+          </Tooltip>
+        }
+        code={`<Tooltip
+  ids={{ trigger: checkboxId }}
+  content="This is the tooltip content"
+>
+  <Checkbox ids={{ root: checkboxId }} label="Welcome" />
 </Tooltip>`}
       />
     </>
