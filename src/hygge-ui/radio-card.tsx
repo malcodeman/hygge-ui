70c23f65d8cf954ac7c834c84b13c2b7d6c68e73
malcodeman <26983729+malcodeman@users.noreply.github.com>
2026-02-01 19:33:10 +0100

    feat: improves radio card docs page

--- src/hygge-ui/radio-card.tsx
+++ src/hygge-ui/radio-card.tsx
@@ -1,57 +1,121 @@
 import { RadioGroup as ArkRadioGroup } from "@ark-ui/react";
-import { cn } from "./cn";
 import { createContext, useContext } from "react";
+import { cva } from "class-variance-authority";
+import { cn } from "./cn";
 
 type ColorPalette = "gray" | "orange" | "teal" | "green" | "red";
+type Size = "xs" | "sm" | "md" | "lg" | "xl";
 
-const RadioCardContext = createContext<{ colorPalette: ColorPalette }>({
+const RadioCardContext = createContext<{
+  colorPalette: ColorPalette;
+  size: Size;
+}>({
   colorPalette: "gray",
+  size: "md",
 });
 
 type RadioCardGroupProps = {
-  colorPalette?: "gray" | "orange" | "teal" | "green" | "red";
+  colorPalette?: ColorPalette;
+  size?: Size;
 } & ArkRadioGroup.RootProps;
 
 export function RadioCardGroup(props: RadioCardGroupProps) {
-  const { colorPalette = "gray", className, ...rest } = props;
+  const {
+    colorPalette = "gray",
+    size = "md",
+    className,
+    children,
+    ...rest
+  } = props;
 
   return (
-    <RadioCardContext.Provider value={{ colorPalette }}>
+    <RadioCardContext.Provider value={{ colorPalette, size }}>
       <ArkRadioGroup.Root
         {...rest}
         className={cn("flex flex-col gap-2", className)}
-      />
+      >
+        <div className="flex items-stretch gap-2">{children}</div>
+      </ArkRadioGroup.Root>
     </RadioCardContext.Provider>
   );
 }
 
+const radioGroupItemVariants = cva(
+  "border-border-subtle flex grow cursor-pointer justify-between gap-2 rounded-sm border transition-colors data-disabled:cursor-not-allowed data-disabled:opacity-50 data-invalid:border-[#fd5454] data-[state=checked]:data-invalid:border-[#fd5454]",
+  {
+    variants: {
+      color: {
+        gray: "data-[state=checked]:border-[#21201C] dark:data-[state=checked]:border-[#eeeeec]",
+        orange: "data-[state=checked]:border-[#eb5e41]",
+        teal: "data-[state=checked]:border-[#0d9488]",
+        green: "data-[state=checked]:border-[#38a169]",
+        red: "data-[state=checked]:border-[#fd5454]",
+      },
+      size: {
+        xs: "p-1",
+        sm: "p-2",
+        md: "p-2",
+        lg: "p-3",
+        xl: "p-3",
+      },
+    },
+    defaultVariants: {
+      color: "gray",
+      size: "md",
+    },
+  },
+);
+const radioGroupItemControlVariants = cva(
+  "border-border-subtle shrink-0 rounded-full border transition-colors",
+  {
+    variants: {
+      color: {
+        gray: "data-[state=checked]:border-[#21201C] data-[state=checked]:bg-[#21201C] dark:data-[state=checked]:border-[#eeeeec] dark:data-[state=checked]:bg-[#eeeeec]",
+        orange:
+          "data-[state=checked]:border-[#eb5e41] data-[state=checked]:bg-[#eb5e41]",
+        teal: "data-[state=checked]:border-[#0d9488] data-[state=checked]:bg-[#0d9488]",
+        green:
+          "data-[state=checked]:border-[#38a169] data-[state=checked]:bg-[#38a169]",
+        red: "data-[state=checked]:border-[#fd5454] data-[state=checked]:bg-[#fd5454]",
+      },
+      size: {
+        xs: "size-3",
+        sm: "size-4",
+        md: "size-5",
+        lg: "size-6",
+        xl: "size-7",
+      },
+    },
+    defaultVariants: {
+      color: "gray",
+      size: "md",
+    },
+  },
+);
+
 type RadioCardProps = {
   label?: React.ReactNode;
   description?: React.ReactNode;
+  icon?: React.ReactNode;
 } & ArkRadioGroup.ItemProps;
 
 export function RadioCard(props: RadioCardProps) {
-  const { label, description, className, ...rest } = props;
-  const { colorPalette } = useContext(RadioCardContext);
-  const hasContent = label || description;
+  const { label, description, icon, className, ...rest } = props;
+  const { colorPalette, size } = useContext(RadioCardContext);
+  const hasContent = label || description || icon;
 
   return (
     <ArkRadioGroup.Item
       {...rest}
       className={cn(
-        "flex cursor-pointer justify-between gap-2 rounded-sm border border-[#E9E8E6] p-2 transition-colors data-disabled:cursor-not-allowed data-disabled:opacity-50 dark:border-[#2a2a28]",
-        {
-          "data-[state=checked]:border-[#21201C]": colorPalette === "gray",
-          "data-[state=checked]:border-[#eb5e41]": colorPalette === "orange",
-          "data-[state=checked]:border-[#0d9488]": colorPalette === "teal",
-          "data-[state=checked]:border-[#38a169]": colorPalette === "green",
-          "data-[state=checked]:border-[#fd5454]": colorPalette === "red",
-        },
+        radioGroupItemVariants({ color: colorPalette, size }),
+        description || icon ? "items-start" : "items-center",
         className,
       )}
     >
       {hasContent ? (
         <div className="flex flex-col">
+          {icon ? <div className="mb-2">{icon}</div> : null}
           {label ? (
             <ArkRadioGroup.ItemText className="text-fg-default text-sm/6 font-semibold">
               {label}
@@ -64,19 +128,7 @@ export function RadioCard(props: RadioCardProps) {
       ) : null}
       <ArkRadioGroup.ItemControl
         className={cn(
-          "size-5 shrink-0 rounded-full border border-[#E9E8E6] transition-colors dark:border-[#2a2a28]",
-          {
-            "data-[state=checked]:border-[#21201C] data-[state=checked]:bg-[#21201C] dark:data-[state=checked]:border-[#eeeeec] dark:data-[state=checked]:bg-[#eeeeec]":
-              colorPalette === "gray",
-            "data-[state=checked]:border-[#eb5e41] data-[state=checked]:bg-[#eb5e41]":
-              colorPalette === "orange",
-            "data-[state=checked]:border-[#0d9488] data-[state=checked]:bg-[#0d9488]":
-              colorPalette === "teal",
-            "data-[state=checked]:border-[#38a169] data-[state=checked]:bg-[#38a169]":
-              colorPalette === "green",
-            "data-[state=checked]:border-[#fd5454] data-[state=checked]:bg-[#fd5454]":
-              colorPalette === "red",
-          },
+          radioGroupItemControlVariants({ color: colorPalette, size }),
         )}
       />
       <ArkRadioGroup.ItemHiddenInput />
--- src/routes/docs.components.radio-card.tsx
+++ src/routes/docs.components.radio-card.tsx
@@ -2,6 +2,9 @@ import { RadioCard, RadioCardGroup } from "hygge-ui/radio-card";
 import { createFileRoute } from "@tanstack/react-router";
 import { PageHeader } from "app/components/page-header";
 import { PagePreviewCodeTabs } from "app/components/page-preview-code-tabs";
+import { Heading } from "hygge-ui/heading";
+import { Code, Text } from "hygge-ui/text";
+import { LuArrowRight, LuCircleOff, LuLock } from "react-icons/lu";
 
 export const Route = createFileRoute("/docs/components/radio-card")({
   component: RadioCardPage,
@@ -22,6 +25,7 @@ function RadioCardPage() {
         description="Used to select an option from list."
         githubLink="https://github.com/malcodeman/hygge-ui/blob/main/src/components/radio-card.tsx"
         className="mb-2"
+        verified
       />
       <PagePreviewCodeTabs
         preview={
@@ -35,6 +39,280 @@ function RadioCardPage() {
   <RadioCard value="nextjs" label="Next.js" />
   <RadioCard value="vite" label="Vite" />
   <RadioCard value="astro" label="Astro" />
+</RadioCardGroup>`}
+      />
+      <Heading level={2} className="mt-10 mb-2">
+        Examples
+      </Heading>
+      <Heading level={3} size="lg" className="mb-2">
+        Description
+      </Heading>
+      <Text className="mb-2">
+        Here&apos;s an example of how to add some further description to the
+        radio card.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <RadioCardGroup>
+            <RadioCard
+              value="nextjs"
+              label="Nextjs"
+              description="Best for apps"
+            />
+            <RadioCard value="vite" label="Vite" description="Best for SPAs" />
+            <RadioCard
+              value="astro"
+              label="Astro"
+              description="Best for static sites"
+            />
+          </RadioCardGroup>
+        }
+        code={`<RadioCardGroup>
+  <RadioCard
+    value="nextjs"
+    label="Nextjs"
+    description="Best for apps"
+  />
+  <RadioCard value="vite" label="Vite" description="Best for SPAs" />
+  <RadioCard
+    value="astro"
+    label="Astro"
+    description="Best for static sites"
+  />
+</RadioCardGroup>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Sizes
+      </Heading>
+      <Text className="mb-2">
+        Pass the <Code>size</Code> prop to the <Code>RadioCardRoot</Code>{" "}
+        component o change the size of the radio card.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <div className="flex flex-col gap-2">
+            <RadioCardGroup size="xs" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup size="sm" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup size="md" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup size="lg" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup size="xl" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+          </div>
+        }
+        code={`<div className="flex flex-col gap-2">
+  <RadioCardGroup size="xs" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup size="sm" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup size="md" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup size="lg" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup size="xl" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+</div>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        States
+      </Heading>
+      <Text className="mb-2">
+        Pass the <Code>disabled</Code> or <Code>invalid</Code> prop to the{" "}
+        <Code>RadioCardGroup</Code> component to change the visual state of the
+        radio.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <div className="flex flex-col gap-2">
+            <RadioCardGroup disabled>
+              <RadioCard
+                value="nextjs"
+                label="Next.js"
+                description="Disabled"
+              />
+              <RadioCard value="vite" label="Vite" description="Disabled" />
+            </RadioCardGroup>
+            <RadioCardGroup disabled defaultValue="vite">
+              <RadioCard
+                value="nextjs"
+                label="Next.js"
+                description="Disabled"
+              />
+              <RadioCard value="vite" label="Vite" description="Disabled" />
+            </RadioCardGroup>
+            <RadioCardGroup readOnly>
+              <RadioCard
+                value="nextjs"
+                label="Next.js"
+                description="Readonly"
+              />
+              <RadioCard value="vite" label="Vite" description="Readonly" />
+            </RadioCardGroup>
+            <RadioCardGroup invalid>
+              <RadioCard value="nextjs" label="Next.js" description="Invalid" />
+              <RadioCard value="vite" label="Vite" description="Invalid" />
+            </RadioCardGroup>
+          </div>
+        }
+        code={`<div className="flex flex-col gap-2">
+  <RadioCardGroup disabled>
+    <RadioCard
+      value="nextjs"
+      label="Next.js"
+      description="Disabled"
+    />
+    <RadioCard value="vite" label="Vite" description="Disabled" />
+  </RadioCardGroup>
+  <RadioCardGroup disabled defaultValue="vite">
+    <RadioCard
+      value="nextjs"
+      label="Next.js"
+      description="Disabled"
+    />
+    <RadioCard value="vite" label="Vite" description="Disabled" />
+  </RadioCardGroup>
+  <RadioCardGroup readOnly>
+    <RadioCard
+      value="nextjs"
+      label="Next.js"
+      description="Readonly"
+    />
+    <RadioCard value="vite" label="Vite" description="Readonly" />
+  </RadioCardGroup>
+  <RadioCardGroup invalid>
+    <RadioCard value="nextjs" label="Next.js" description="Invalid" />
+    <RadioCard value="vite" label="Vite" description="Invalid" />
+  </RadioCardGroup>
+</div>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Colors
+      </Heading>
+      <Text className="mb-2">
+        Pass the <Code>colorPalette</Code> prop to the{" "}
+        <Code>RadioCardGroup</Code> component to change the color of the radio
+        card.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <div className="flex flex-col gap-2">
+            <RadioCardGroup colorPalette="gray" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup colorPalette="orange" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup colorPalette="teal" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup colorPalette="green" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+            <RadioCardGroup colorPalette="red" defaultValue="nextjs">
+              <RadioCard value="nextjs" label="Next.js" />
+              <RadioCard value="vite" label="Vite" />
+            </RadioCardGroup>
+          </div>
+        }
+        code={`<div className="flex flex-col gap-2">
+  <RadioCardGroup colorPalette="gray" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup colorPalette="orange" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup colorPalette="teal" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup colorPalette="green" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+  <RadioCardGroup colorPalette="red" defaultValue="nextjs">
+    <RadioCard value="nextjs" label="Next.js" />
+    <RadioCard value="vite" label="Vite" />
+  </RadioCardGroup>
+</div>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Icon
+      </Heading>
+      <Text className="mb-2">
+        Pass the <Code>icon</Code> prop to the <Code>RadioCard</Code> component
+        to render a custom icon inside the radio card.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <RadioCardGroup>
+            <RadioCard
+              icon={<LuArrowRight />}
+              value="allow"
+              label="Allow"
+              description="This user can access the system"
+            />
+            <RadioCard
+              icon={<LuCircleOff />}
+              value="deny"
+              label="Deny"
+              description="This user will be denied access to the system"
+            />
+            <RadioCard
+              icon={<LuLock />}
+              value="lock"
+              label="Lock"
+              description="This user will be locked out of the system"
+            />
+          </RadioCardGroup>
+        }
+        code={`<RadioCardGroup>
+  <RadioCard
+    icon={<LuArrowRight />}
+    value="allow"
+    label="Allow"
+    description="This user can access the system"
+  />
+  <RadioCard
+    icon={<LuCircleOff />}
+    value="deny"
+    label="Deny"
+    description="This user will be denied access to the system"
+  />
+  <RadioCard
+    icon={<LuLock />}
+    value="lock"
+    label="Lock"
+    description="This user will be locked out of the system"
+  />
 </RadioCardGroup>`}
       />
     </>
