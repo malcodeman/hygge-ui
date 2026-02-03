920fbcd1c4c602414ccfaab94aaf44bab8aa2f20
malcodeman <26983729+malcodeman@users.noreply.github.com>
2026-02-03 13:27:19 +0100

    feat: improves steps doc page

--- src/hygge-ui/steps.tsx
+++ src/hygge-ui/steps.tsx
@@ -1,14 +1,58 @@
 import { Steps as ArkSteps } from "@ark-ui/react";
+import { createContext, useContext } from "react";
+import { cva } from "class-variance-authority";
 import { cn } from "./cn";
 
-export function StepsRoot(props: ArkSteps.RootProps) {
-  const { className, ...rest } = props;
+const indicatorVariants = cva(
+  "border-border-subtle text-fg-muted flex items-center justify-center rounded-full border font-semibold transition-colors data-current:border-[#21201C] data-current:bg-[#21201C] data-current:text-white dark:data-current:bg-[#eeeeec] dark:data-current:text-[#191918]",
+  {
+    variants: {
+      size: {
+        xs: "size-6 text-xs",
+        sm: "size-8 text-xs",
+        md: "size-10 text-sm",
+        lg: "size-11 text-base",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  },
+);
+const titleVariants = cva("text-fg-default", {
+  variants: {
+    size: {
+      xs: "text-xs",
+      sm: "text-xs",
+      md: "text-sm",
+      lg: "text-base",
+      xl: "text-lg",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+type Size = "xs" | "sm" | "md" | "lg";
+type StepsRootProps = {
+  size?: Size;
+} & ArkSteps.RootProps;
+
+const StepsContext = createContext<{ size: Size }>({
+  size: "md",
+});
+
+export function StepsRoot(props: StepsRootProps) {
+  const { className, size = "md", ...rest } = props;
 
   return (
-    <ArkSteps.Root
-      {...rest}
-      className={cn("flex w-full flex-col gap-4", className)}
-    />
+    <StepsContext.Provider value={{ size }}>
+      <ArkSteps.Root
+        {...rest}
+        className={cn("flex w-full flex-col gap-4", className)}
+      />
+    </StepsContext.Provider>
   );
 }
 
@@ -26,8 +70,13 @@ export function StepsList(props: ArkSteps.ListProps) {
   );
 }
 
-export function StepsItem(props: ArkSteps.ItemProps) {
-  const { className, index, title, ...rest } = props;
+type StepsItemProps = ArkSteps.ItemProps & {
+  icon?: React.ReactNode;
+};
+
+export function StepsItem(props: StepsItemProps) {
+  const { className, index, title, icon, ...rest } = props;
+  const { size } = useContext(StepsContext);
 
   return (
     <ArkSteps.Item
@@ -36,10 +85,12 @@ export function StepsItem(props: ArkSteps.ItemProps) {
       className={cn("flex grow items-center gap-2", className)}
     >
       <ArkSteps.Trigger className="flex items-center gap-2 focus:outline-2 focus:-outline-offset-1">
-        <ArkSteps.Indicator className="border-border-subtle text-fg-muted flex size-8 items-center justify-center rounded-full border text-sm font-semibold transition-colors data-current:border-[#21201C] data-current:bg-[#21201C] data-current:text-white dark:data-current:bg-[#eeeeec] dark:data-current:text-[#191918]">
-          {index + 1}
+        <ArkSteps.Indicator className={indicatorVariants({ size })}>
+          {icon ? icon : index + 1}
         </ArkSteps.Indicator>
-        <span className="text-fg-muted text-sm">{title}</span>
+        {title ? (
+          <span className={cn(titleVariants({ size }))}>{title}</span>
+        ) : null}
       </ArkSteps.Trigger>
       <ArkSteps.Separator className="h-px grow bg-[#cfceca] dark:bg-[#494844]" />
     </ArkSteps.Item>
@@ -47,14 +98,7 @@ export function StepsItem(props: ArkSteps.ItemProps) {
 }
 
 export function StepsContent(props: ArkSteps.ContentProps) {
-  const { className, ...rest } = props;
-
-  return (
-    <ArkSteps.Content
-      {...rest}
-      className={cn("focus:outline-2 focus:-outline-offset-1", className)}
-    />
-  );
+  return <ArkSteps.Content {...props} />;
 }
 
 export function StepsCompletedContent(props: ArkSteps.CompletedContentProps) {
--- src/routes/docs.components.steps.tsx
+++ src/routes/docs.components.steps.tsx
@@ -11,6 +11,9 @@ import {
 import { createFileRoute } from "@tanstack/react-router";
 import { PageHeader } from "app/components/page-header";
 import { PagePreviewCodeTabs } from "app/components/page-preview-code-tabs";
+import { Heading } from "hygge-ui/heading";
+import { Code, Text } from "hygge-ui/text";
+import { LuCalendar, LuUser, LuWallet } from "react-icons/lu";
 
 export const Route = createFileRoute("/docs/components/steps")({
   component: StepsPage,
@@ -31,25 +34,26 @@ function StepsPage() {
         description="Used to indicate progress through a multi-step process."
         githubLink="https://github.com/malcodeman/hygge-ui/blob/main/src/components/steps.tsx"
         className="mb-2"
+        verified
       />
       <PagePreviewCodeTabs
         preview={
           <StepsRoot count={3}>
             <StepsList>
-              <StepsItem index={0} title="First" />
-              <StepsItem index={1} title="Second" />
-              <StepsItem index={2} title="Third" />
+              <StepsItem index={0} title="Step 1" />
+              <StepsItem index={1} title="Step 2" />
+              <StepsItem index={2} title="Step 3" />
             </StepsList>
-            <StepsContent index={0}>First - Contact Info</StepsContent>
-            <StepsContent index={1}>Second - Date & Time</StepsContent>
-            <StepsContent index={2}>Third - Select Rooms</StepsContent>
+            <StepsContent index={0}>Step 1 description</StepsContent>
+            <StepsContent index={1}>Step 2 description</StepsContent>
+            <StepsContent index={2}>Step 3 description</StepsContent>
             <StepsCompletedContent>
-              Steps Complete - Thank you for filling out the form!
+              All steps are complete!
             </StepsCompletedContent>
             <div className="flex gap-2">
               <StepsPrevTrigger asChild>
                 <Button variant="outline" size="sm">
-                  Back
+                  Prev
                 </Button>
               </StepsPrevTrigger>
               <StepsNextTrigger asChild>
@@ -62,20 +66,299 @@ function StepsPage() {
         }
         code={`<StepsRoot count={3}>
   <StepsList>
-    <StepsItem index={0} title="First" />
-    <StepsItem index={1} title="Second" />
-    <StepsItem index={2} title="Third" />
+    <StepsItem index={0} title="Step 1" />
+    <StepsItem index={1} title="Step 2" />
+    <StepsItem index={2} title="Step 3" />
   </StepsList>
-  <StepsContent index={0}>First - Contact Info</StepsContent>
-  <StepsContent index={1}>Second - Date & Time</StepsContent>
-  <StepsContent index={2}>Third - Select Rooms</StepsContent>
+  <StepsContent index={0}>Step 1 description</StepsContent>
+  <StepsContent index={1}>Step 2 description</StepsContent>
+  <StepsContent index={2}>Step 3 description</StepsContent>
   <StepsCompletedContent>
-    Steps Complete - Thank you for filling out the form!
+    All steps are complete!
   </StepsCompletedContent>
   <div className="flex gap-2">
     <StepsPrevTrigger asChild>
       <Button variant="outline" size="sm">
-        Back
+        Prev
+      </Button>
+    </StepsPrevTrigger>
+    <StepsNextTrigger asChild>
+      <Button variant="outline" size="sm">
+        Next
+      </Button>
+    </StepsNextTrigger>
+  </div>
+</StepsRoot>`}
+      />
+      <Heading level={2} className="mt-10 mb-2">
+        Examples
+      </Heading>
+      <Heading level={3} size="lg" className="mb-2">
+        Sizes
+      </Heading>
+      <Text className="mb-2">
+        Use the <Code>size</Code> prop to change the size of the steps
+        component.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <div className="flex flex-col gap-2">
+            <StepsRoot size="xs" count={3}>
+              <StepsList>
+                <StepsItem index={0} title="Step 1" />
+                <StepsItem index={1} title="Step 2" />
+                <StepsItem index={2} title="Step 3" />
+              </StepsList>
+              <StepsContent index={0}>Step 1 description</StepsContent>
+              <StepsContent index={1}>Step 2 description</StepsContent>
+              <StepsContent index={2}>Step 3 description</StepsContent>
+              <StepsCompletedContent>
+                All steps are complete!
+              </StepsCompletedContent>
+              <div className="flex gap-2">
+                <StepsPrevTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Prev
+                  </Button>
+                </StepsPrevTrigger>
+                <StepsNextTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Next
+                  </Button>
+                </StepsNextTrigger>
+              </div>
+            </StepsRoot>
+            <StepsRoot size="sm" count={3}>
+              <StepsList>
+                <StepsItem index={0} title="Step 1" />
+                <StepsItem index={1} title="Step 2" />
+                <StepsItem index={2} title="Step 3" />
+              </StepsList>
+              <StepsContent index={0}>Step 1 description</StepsContent>
+              <StepsContent index={1}>Step 2 description</StepsContent>
+              <StepsContent index={2}>Step 3 description</StepsContent>
+              <StepsCompletedContent>
+                All steps are complete!
+              </StepsCompletedContent>
+              <div className="flex gap-2">
+                <StepsPrevTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Prev
+                  </Button>
+                </StepsPrevTrigger>
+                <StepsNextTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Next
+                  </Button>
+                </StepsNextTrigger>
+              </div>
+            </StepsRoot>
+            <StepsRoot size="md" count={3}>
+              <StepsList>
+                <StepsItem index={0} title="Step 1" />
+                <StepsItem index={1} title="Step 2" />
+                <StepsItem index={2} title="Step 3" />
+              </StepsList>
+              <StepsContent index={0}>Step 1 description</StepsContent>
+              <StepsContent index={1}>Step 2 description</StepsContent>
+              <StepsContent index={2}>Step 3 description</StepsContent>
+              <StepsCompletedContent>
+                All steps are complete!
+              </StepsCompletedContent>
+              <div className="flex gap-2">
+                <StepsPrevTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Prev
+                  </Button>
+                </StepsPrevTrigger>
+                <StepsNextTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Next
+                  </Button>
+                </StepsNextTrigger>
+              </div>
+            </StepsRoot>
+            <StepsRoot size="lg" count={3}>
+              <StepsList>
+                <StepsItem index={0} title="Step 1" />
+                <StepsItem index={1} title="Step 2" />
+                <StepsItem index={2} title="Step 3" />
+              </StepsList>
+              <StepsContent index={0}>Step 1 description</StepsContent>
+              <StepsContent index={1}>Step 2 description</StepsContent>
+              <StepsContent index={2}>Step 3 description</StepsContent>
+              <StepsCompletedContent>
+                All steps are complete!
+              </StepsCompletedContent>
+              <div className="flex gap-2">
+                <StepsPrevTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Prev
+                  </Button>
+                </StepsPrevTrigger>
+                <StepsNextTrigger asChild>
+                  <Button variant="outline" size="sm">
+                    Next
+                  </Button>
+                </StepsNextTrigger>
+              </div>
+            </StepsRoot>
+          </div>
+        }
+        code={`<div className="flex flex-col gap-2">
+  <StepsRoot size="xs" count={3}>
+    <StepsList>
+      <StepsItem index={0} title="Step 1" />
+      <StepsItem index={1} title="Step 2" />
+      <StepsItem index={2} title="Step 3" />
+    </StepsList>
+    <StepsContent index={0}>Step 1 description</StepsContent>
+    <StepsContent index={1}>Step 2 description</StepsContent>
+    <StepsContent index={2}>Step 3 description</StepsContent>
+    <StepsCompletedContent>
+      All steps are complete!
+    </StepsCompletedContent>
+    <div className="flex gap-2">
+      <StepsPrevTrigger asChild>
+        <Button variant="outline" size="sm">
+          Prev
+        </Button>
+      </StepsPrevTrigger>
+      <StepsNextTrigger asChild>
+        <Button variant="outline" size="sm">
+          Next
+        </Button>
+      </StepsNextTrigger>
+    </div>
+  </StepsRoot>
+  <StepsRoot size="sm" count={3}>
+    <StepsList>
+      <StepsItem index={0} title="Step 1" />
+      <StepsItem index={1} title="Step 2" />
+      <StepsItem index={2} title="Step 3" />
+    </StepsList>
+    <StepsContent index={0}>Step 1 description</StepsContent>
+    <StepsContent index={1}>Step 2 description</StepsContent>
+    <StepsContent index={2}>Step 3 description</StepsContent>
+    <StepsCompletedContent>
+      All steps are complete!
+    </StepsCompletedContent>
+    <div className="flex gap-2">
+      <StepsPrevTrigger asChild>
+        <Button variant="outline" size="sm">
+          Prev
+        </Button>
+      </StepsPrevTrigger>
+      <StepsNextTrigger asChild>
+        <Button variant="outline" size="sm">
+          Next
+        </Button>
+      </StepsNextTrigger>
+    </div>
+  </StepsRoot>
+  <StepsRoot size="md" count={3}>
+    <StepsList>
+      <StepsItem index={0} title="Step 1" />
+      <StepsItem index={1} title="Step 2" />
+      <StepsItem index={2} title="Step 3" />
+    </StepsList>
+    <StepsContent index={0}>Step 1 description</StepsContent>
+    <StepsContent index={1}>Step 2 description</StepsContent>
+    <StepsContent index={2}>Step 3 description</StepsContent>
+    <StepsCompletedContent>
+      All steps are complete!
+    </StepsCompletedContent>
+    <div className="flex gap-2">
+      <StepsPrevTrigger asChild>
+        <Button variant="outline" size="sm">
+          Prev
+        </Button>
+      </StepsPrevTrigger>
+      <StepsNextTrigger asChild>
+        <Button variant="outline" size="sm">
+          Next
+        </Button>
+      </StepsNextTrigger>
+    </div>
+  </StepsRoot>
+  <StepsRoot size="lg" count={3}>
+    <StepsList>
+      <StepsItem index={0} title="Step 1" />
+      <StepsItem index={1} title="Step 2" />
+      <StepsItem index={2} title="Step 3" />
+    </StepsList>
+    <StepsContent index={0}>Step 1 description</StepsContent>
+    <StepsContent index={1}>Step 2 description</StepsContent>
+    <StepsContent index={2}>Step 3 description</StepsContent>
+    <StepsCompletedContent>
+      All steps are complete!
+    </StepsCompletedContent>
+    <div className="flex gap-2">
+      <StepsPrevTrigger asChild>
+        <Button variant="outline" size="sm">
+          Prev
+        </Button>
+      </StepsPrevTrigger>
+      <StepsNextTrigger asChild>
+        <Button variant="outline" size="sm">
+          Next
+        </Button>
+      </StepsNextTrigger>
+    </div>
+  </StepsRoot>
+</div>`}
+      />
+      <Heading level={3} size="lg" className="mt-10 mb-2">
+        Icon
+      </Heading>
+      <Text className="mb-2">
+        Pass the <Code>icon</Code> prop to the <Code>StepsItem</Code> component
+        to display an icon.
+      </Text>
+      <PagePreviewCodeTabs
+        preview={
+          <StepsRoot count={3}>
+            <StepsList>
+              <StepsItem index={0} icon={<LuUser />} />
+              <StepsItem index={1} icon={<LuWallet />} />
+              <StepsItem index={2} icon={<LuCalendar />} />
+            </StepsList>
+            <StepsContent index={0}>Contact Details</StepsContent>
+            <StepsContent index={1}>Payment</StepsContent>
+            <StepsContent index={2}>Book an Appointment</StepsContent>
+            <StepsCompletedContent>
+              All steps are complete!
+            </StepsCompletedContent>
+            <div className="flex gap-2">
+              <StepsPrevTrigger asChild>
+                <Button variant="outline" size="sm">
+                  Prev
+                </Button>
+              </StepsPrevTrigger>
+              <StepsNextTrigger asChild>
+                <Button variant="outline" size="sm">
+                  Next
+                </Button>
+              </StepsNextTrigger>
+            </div>
+          </StepsRoot>
+        }
+        code={`<StepsRoot count={3}>
+  <StepsList>
+    <StepsItem index={0} icon={<LuUser />} />
+    <StepsItem index={1} icon={<LuWallet />} />
+    <StepsItem index={2} icon={<LuCalendar />} />
+  </StepsList>
+  <StepsContent index={0}>Contact Details</StepsContent>
+  <StepsContent index={1}>Payment</StepsContent>
+  <StepsContent index={2}>Book an Appointment</StepsContent>
+  <StepsCompletedContent>
+    All steps are complete!
+  </StepsCompletedContent>
+  <div className="flex gap-2">
+    <StepsPrevTrigger asChild>
+      <Button variant="outline" size="sm">
+        Prev
       </Button>
     </StepsPrevTrigger>
     <StepsNextTrigger asChild>
