46d7d3693deb470d0fae4c7947e864f12da0f19a
malcodeman <26983729+malcodeman@users.noreply.github.com>
2026-06-24 20:32:54 +0200

    small fix

--- src/hygge-ui/combobox.tsx
+++ src/hygge-ui/combobox.tsx
@@ -69,7 +69,7 @@ export function ComboboxInput(props: ArkCombobox.InputProps) {
       <ArkCombobox.Input {...props} asChild>
         <Input variant={variant} size={size} className="pr-16" />
       </ArkCombobox.Input>
-      <div className="absolute right-2 flex items-center gap-1">
+      <div className="absolute inset-e-0 flex gap-1 px-3">
         <ArkCombobox.ClearTrigger className="cursor-pointer disabled:cursor-not-allowed disabled:opacity-50">
           <LuX size={16} />
         </ArkCombobox.ClearTrigger>
